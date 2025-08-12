ec14b7434f50864d7c986f2119a4e06b714a88d0
Tejasv Chauhan <149497252+01tejasv@users.noreply.github.com>
2025-08-13 03:46:59 +0530

    Update test_pinecone.py

--- test_pinecone.py
+++ test_pinecone.py
@@ -1,3 +1,4 @@
 from pinecone import Client
 
-print("Pinecone Client imported successfully!")
+pc = Client(api_key="pcsk_5TezHr_5FS18xfebbQxaGZwSRUELygH9RUq7Hnor9u7FdDDYoq4ztLAS2Fv2LC5W19Z4Me", environment="aped-4627-b74a")
+print(pc.list_indexes())
